c1b459dfad21884ce9f7899b5df6c0af2a030371
evant1986 <44884012+evant1986@users.noreply.github.com>
2024-11-02 01:00:27 -0400

    new new

--- out/production/CardShuff2/README.md
+++
@@ -1,1 +0,0 @@
-# CardShuff2
--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CardShuff2"},{"p":"<Unnamed>","l":"Deck"}];updateSearchResults();
